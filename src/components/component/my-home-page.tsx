beb69d5936ba6a9a54311f54b955fa79af7a083b
Kevin <brittokevin.04@gmail.com>
2024-09-07 12:10:21 +0530

    Changes

--- src/components/component/my-home-page.tsx
+++ src/components/component/my-home-page.tsx
@@ -1,21 +1,3 @@
-/**
- * @see https://v0.dev/t/AO57z2zNZmF
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
-
-/** Add fonts into your Next.js project:
-
-import { Libre_Franklin } from 'next/font/google'
-
-libre_franklin({
-  subsets: ['latin'],
-  display: 'swap',
-})
-
-To read more about using these font, please visit the Next.js documentation:
-- App Directory: https://nextjs.org/docs/app/building-your-application/optimizing/fonts
-- Pages Directory: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
-**/
 "use client";
 import Image from "next/image";
 import React from "react";
